0a7080da9a3979779c3a60e3580377f0cbebe61e
Michael Kutý <6du1ro.n@gmail.com>
2017-01-24 15:02:35 +0100

    Fix boto lib.

--- airflow/files/start-service.sh
+++ airflow/files/start-service.sh
@@ -5,4 +5,8 @@
 
 export AIRFLOW_HOME="{{ server.dir.home }}"
 
+# do not load /etc/boto.cfg with Python 3 incompatible plugin
+# https://github.com/travis-ci/travis-ci/issues/5246#issuecomment-166460882
+export BOTO_CONFIG=/doesnotexist
+
 exec $1
